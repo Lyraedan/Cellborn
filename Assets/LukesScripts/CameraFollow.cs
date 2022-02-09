4ab173b8dfec5310b1cd24d6e5ccaa291f1e3333
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2022-02-09 19:49:00 +0000

    Camera follow

---
+++ Assets/LukesScripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 distanceFrom = new Vector3(1.5f, -5f, 10f);
+
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.x -= distanceFrom.x;
+        pos.y -= distanceFrom.y;
+        pos.z -= distanceFrom.z;
+        transform.position = pos;
+    }
+}
