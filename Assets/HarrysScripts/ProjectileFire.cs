11601b865b29c1a7446e4a90e616d51f5c29eb90
SimpsonGames <harrysimpson2001@gmail.com>
2022-02-11 17:49:53 +0000

    Projectile Fire Player Movement (11/02/2022 17:47)
    Altered the aiming system so the player faces the direction they are aiming.
    
    Additional: Imported the triple-barrelled shotgun into a scene and added a test smoke particle system.

--- Assets/HarrysScripts/ProjectileFire.cs
+++ Assets/HarrysScripts/ProjectileFire.cs
@@ -5,21 +5,16 @@ using UnityEngine;
 public class ProjectileFire : MonoBehaviour
 {
     public GameObject projectile;
-    public Transform projectileSpawn;
-    public GameObject target;
-
-    float yRot;
+    public Transform projectileSpawn, player, target;
 
     void Update()
-    {
-        projectileSpawn.transform.LookAt(target.transform);
-        yRot = projectileSpawn.transform.rotation.y;
-        yRot = Mathf.Clamp(projectileSpawn.transform.rotation.y, -80f, 80f);
-        projectileSpawn.transform.localRotation = Quaternion.Euler(transform.rotation.x, yRot, transform.rotation.z);
-        
+    {                
+        player.LookAt(target.position);
+        player.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject projInstance = Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+            GameObject projInstance = Instantiate(projectile, transform.position, transform.rotation);
         }
     }
 }
