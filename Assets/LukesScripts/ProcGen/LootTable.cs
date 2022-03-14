1052df9eae49a66965c55eb11092a1006dca7301
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2022-03-14 21:11:15 +0000

    Loot table push

---
+++ Assets/LukesScripts/ProcGen/LootTable.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootTable : MonoBehaviour
+{
+    [Tooltip("The higher this value. The less likely loot is to spawn")] public float chancesOfSpawningLoot = 1;
+    public List<Loot> tables = new List<Loot>();
+
+    void Start()
+    {
+        float spawnChance = Random.Range(0, chancesOfSpawningLoot);
+        bool doSpawn = Mathf.RoundToInt(spawnChance) == 0;
+
+        if (doSpawn)
+        {
+            int selected = Random.Range(0, tables.Count);
+            var table = tables[selected];
+            table.SpawnRandomPrefab(this);
+        }
+    }
+
+    public GameObject Spawn(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        return Instantiate(prefab, position, rotation);
+    }
+}
+
+[System.Serializable]
+public class LootPrefab
+{
+    public GameObject prefab;
+    public Vector3 rotation = Vector3.zero;
+}
+
+[System.Serializable]
+public class Loot
+{
+    public Transform spawnpoint;
+    public List<LootPrefab> possiblePrefabs = new List<LootPrefab>();
+
+    public GameObject SpawnPrefab(int index, LootTable table)
+    {
+        var loot = possiblePrefabs[index];
+        return table.Spawn(loot.prefab, spawnpoint.position, Quaternion.Euler(loot.rotation));
+    }
+
+    public GameObject SpawnRandomPrefab(LootTable table)
+    {
+        int index = Random.Range(0, possiblePrefabs.Count);
+        return SpawnPrefab(index, table);
+    }
+}
