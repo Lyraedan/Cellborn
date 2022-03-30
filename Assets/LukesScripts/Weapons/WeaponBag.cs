5593953d19bc72ed9126f692bff387f1a1499317
MissMatilde <78206681+MissMatilde@users.noreply.github.com>
2022-03-30 23:02:52 +0100

    Weapon Bag

---
+++ Assets/LukesScripts/Weapons/WeaponBag.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class WeaponBag : MonoBehaviour
+{
+    public static WeaponBag instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public List<GameObject> weaponBag = new List<GameObject>();
+    public List<GameObject> backupBag = new List<GameObject>();
+
+    private void Start()
+    {
+        RefillBag();
+    }
+
+    public void RefillBag()
+    {
+        weaponBag = backupBag.ToList();
+    }
+}
