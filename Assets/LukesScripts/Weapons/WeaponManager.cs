07bf86d7366d13f3a66c9f973f26098c4720c814
MissMatilde <78206681+MissMatilde@users.noreply.github.com>
2022-04-15 00:13:37 +0100

    Remove Weapon

--- Assets/LukesScripts/Weapons/WeaponManager.cs
+++ Assets/LukesScripts/Weapons/WeaponManager.cs
@@ -96,8 +96,7 @@ public class WeaponManager : MonoBehaviour
         }
         currentlyHeldWeapons[2] = FindWeapon(0); // Pebbles 
 
-        currentlyHeldWeapons[0] = FindWeapon(7);
-        currentlyHeldWeapons[1] = FindWeapon(4);
+        //currentlyHeldWeapons[0] = FindWeapon(7);
 
         for (int i = 0; i < currentlyHeldWeapons.Count; i++)
         {
