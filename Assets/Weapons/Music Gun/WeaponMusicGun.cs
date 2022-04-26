794451496f8f2f8502cdf9869e5da9618591c66e
MissMatilde <78206681+MissMatilde@users.noreply.github.com>
2022-04-26 20:13:32 +0100

    Music Gun Fire Speed

--- Assets/Weapons/Music Gun/WeaponMusicGun.cs
+++ Assets/Weapons/Music Gun/WeaponMusicGun.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class WeaponMusicGun : WeaponBase
 {
+    public int AccuracyRange;
+    public float accuracy;
+    System.Random random = new System.Random();
+
     public override void Init()
     {
-
+        
     }
 
     public override void Tick()
     {
+        if (Input.GetButton("Fire1"))
+        {
+            if (secondsBetweenShots >= 0.06f)
+            {
+                secondsBetweenShots = secondsBetweenShots - 0.0001f;
+            }
+        }
+        else
+        {
+            secondsBetweenShots = 0.2f;
+        }
     }
 
     public override void Fire()
     {
-        
+
     }
 }
