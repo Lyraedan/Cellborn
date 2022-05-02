58ffc8cf0ed01ccb1dde89b9dacbaf81a034eba5
SimpsonGames <harrysimpson2001@gmail.com>
2022-05-03 00:16:22 +0100

    Tried to account for 4:3 res (didn't really work)

--- Assets/HarrysScripts/UI/UIController.cs
+++ Assets/HarrysScripts/UI/UIController.cs
@@ -25,6 +25,10 @@ public class UIController : MonoBehaviour
     [Header("Minimap")]
     public Image minimapBG;
 
+    [Header("Effects")]
+    public Image damageIndicator;
+    public Image defenseIndicator;
+
     void Start()
     {
         Minimap.instance.OnMapGenerated += () =>
@@ -33,5 +37,10 @@ public class UIController : MonoBehaviour
             minimapBG.rectTransform.position = Minimap.instance.canvas.rectTransform.position;
             minimapBG.rectTransform.sizeDelta = new Vector2(Minimap.instance.canvas.sprite.rect.width * 4, Minimap.instance.canvas.sprite.rect.height * 4);
         };
+
+        Debug.Log(Screen.currentResolution.width + "," + Screen.currentResolution.height);
+
+        damageIndicator.rectTransform.sizeDelta = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+        defenseIndicator.rectTransform.sizeDelta = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
     }
 }
