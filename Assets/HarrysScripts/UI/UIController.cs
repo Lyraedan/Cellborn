e549648716e65421c2b044a0f29a660dc9fca43d
SimpsonGames <harrysimpson2001@gmail.com>
2022-05-26 14:05:34 +0100

    Removed defunked code from UIController

--- Assets/HarrysScripts/UI/UIController.cs
+++ Assets/HarrysScripts/UI/UIController.cs
@@ -25,10 +25,6 @@ public class UIController : MonoBehaviour
     [Header("Minimap")]
     public Image minimapBG;
 
-    [Header("Effects")]
-    public Image damageIndicator;
-    public Image defenseIndicator;
-
     void Start()
     {
         Minimap.instance.OnMapGenerated += () =>
@@ -37,10 +33,5 @@ public class UIController : MonoBehaviour
             minimapBG.rectTransform.position = Minimap.instance.canvas.rectTransform.position;
             minimapBG.rectTransform.sizeDelta = new Vector2(Minimap.instance.canvas.sprite.rect.width * 4, Minimap.instance.canvas.sprite.rect.height * 4);
         };
-
-        /* Debug.Log(Screen.currentResolution.width + "," + Screen.currentResolution.height);
-
-        damageIndicator.rectTransform.sizeDelta = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
-        defenseIndicator.rectTransform.sizeDelta = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height); */
     }
 }
