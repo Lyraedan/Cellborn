1e69476e94a5b5b4ef1c3a517622024699763e17
thomasldw <100153103+thomasldw@users.noreply.github.com>
2022-05-29 16:33:55 +0100

    Potion spinning and floating animation

---
+++ Assets/thomas' scripts/PotionHeight.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionHeight : MonoBehaviour
+{
+      public GameObject potion; 
+    public float Height = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (potion.transform.position.y < Height)
+        {
+            potion.transform.position = new Vector3(potion.transform.position.x, Height, potion.transform.position.z);
+        }
+    }
+}
