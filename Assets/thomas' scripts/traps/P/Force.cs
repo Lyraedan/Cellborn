216403e8ddade45403e2bc8f56137f1842d988bd
thomasldw <100153103+thomasldw@users.noreply.github.com>
2022-06-05 23:00:19 +0100

    Finished Traps

---
+++ Assets/thomas' scripts/traps/P/Force.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force : MonoBehaviour
+{
+  Rigidbody m_Rigidbody;
+    public float m_Thrust = 20f;
+
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            m_Rigidbody.AddForce(transform.up * m_Thrust);
+    }
+}
