6d93620dd3c960cf5a717453dc6acce2e83ad8f7
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2022-06-12 23:21:22 +0100

    Prop null check

--- Assets/LukesScripts/ProcGen/Prop.cs
+++ Assets/LukesScripts/ProcGen/Prop.cs
@@ -12,19 +12,22 @@ public class Prop : MonoBehaviour
         var gridCellCoords = RoomGenerator.instance.navAgent.PositionAsGridCoordinates(transform.position);
         cell = RoomGenerator.instance.navAgent.GetGridCellAt((int)transform.position.x, 0, (int)transform.position.z);
 
-        if (cell.flag.Equals(GridCell.GridFlag.OCCUPIED))
-            cell.flag = GridCell.GridFlag.PROP;
-        else if (cell.flag.Equals(GridCell.GridFlag.WALL))
-            cell.flag = GridCell.GridFlag.WALL_PROP;
-        else if (cell.flag.Equals(GridCell.GridFlag.WALKABLE))
+        if (cell != null)
         {
-            cell = RoomGenerator.instance.floorMesh.GetClosestEdgeAt(transform.position).cell;
-            cell.flag = GridCell.GridFlag.WALL_PROP;
-        }
+            if (cell.flag.Equals(GridCell.GridFlag.OCCUPIED))
+                cell.flag = GridCell.GridFlag.PROP;
+            else if (cell.flag.Equals(GridCell.GridFlag.WALL))
+                cell.flag = GridCell.GridFlag.WALL_PROP;
+            else if (cell.flag.Equals(GridCell.GridFlag.WALKABLE))
+            {
+                cell = RoomGenerator.instance.floorMesh.GetClosestEdgeAt(transform.position).cell;
+                cell.flag = GridCell.GridFlag.WALL_PROP;
+            }
 
-        // After all the checks fuckin remove
-        if (cell.flag.Equals(GridCell.GridFlag.OCCUPIED))
-            Remove();
+            // After all the checks fuckin remove
+            if (cell.flag.Equals(GridCell.GridFlag.OCCUPIED))
+                Remove();
+        }
     }
 
     public void Remove()
