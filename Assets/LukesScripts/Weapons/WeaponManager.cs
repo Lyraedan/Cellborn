ab0490b8a187cd38f14ee212a93fa5fa772cdb75
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2022-06-12 22:46:49 +0100

    Remvoed guns from inventory, moved pause button to actually be on the xbox pause button

--- Assets/LukesScripts/Weapons/WeaponManager.cs
+++ Assets/LukesScripts/Weapons/WeaponManager.cs
@@ -238,8 +238,8 @@ public class WeaponManager : MonoBehaviour
             currentlyHeldWeapons[i] = FindWeapon(-1);
         }
         currentlyHeldWeapons[2] = FindWeapon(0); // Pebbles 
-        currentlyHeldWeapons[0] = FindWeapon(9);
-        currentlyHeldWeapons[1] = FindWeapon(7);
+        //currentlyHeldWeapons[0] = FindWeapon(9);
+        //currentlyHeldWeapons[1] = FindWeapon(7);
         //currentlyHeldWeapons[1] = FindWeapon(2);
 
         for (int i = 0; i < currentlyHeldWeapons.Count; i++)
