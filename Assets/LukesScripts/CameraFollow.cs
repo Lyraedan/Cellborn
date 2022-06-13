88fa6c32115f406eaa6feb5fa2920a0e8172db13
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2022-06-13 20:37:54 +0100

    Changed camera update from Update to LateUpdate

--- Assets/LukesScripts/CameraFollow.cs
+++ Assets/LukesScripts/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
     public GameObject player;
     public Vector3 distanceFrom = new Vector3(0.5f, -2.5f, 4.5f);
 
-    void Update()
+    void LateUpdate()
     {
         if (player != null)
         {
